6350ff1f775aad72580893b6dca2c4c134594632
Radek <radek.dumin@gmail.com>
2025-09-27 18:41:29 +0200

    add header

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -30,7 +30,7 @@ const user ={name: 'stefan', email:"r@r.pl"}
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
+                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm ">{user.name[0]}</AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
                         <span className="text-base font-medium text-gray-400">

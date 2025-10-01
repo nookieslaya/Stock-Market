3b69f488ebf966aa4dcec649caab6a9c891b1ffe
Radek <radek.dumin@gmail.com>
2025-10-01 17:10:18 +0200

    watchlist

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -45,7 +45,7 @@ const UserDropdown = ({user, initialStocks}: { user: User, initialStocks: StockW
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4"/>
+                            <AvatarImage src=""/>
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user.name[0]}
                             </AvatarFallback>
@@ -60,7 +60,8 @@ const UserDropdown = ({user, initialStocks}: { user: User, initialStocks: StockW
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator className="bg-gray-600"/>
                 <Link href="/watchlist">
-                    <DropdownMenuItem className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                    <DropdownMenuItem
+                        className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                         Watchlist
                     </DropdownMenuItem>
                 </Link>
